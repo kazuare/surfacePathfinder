54530a60d9fb0f5e2f1112c36d8752ffd673d109
kazuare <kazuare@mail.ru>
2017-04-11 03:34:15 +0300

    improve drawing speed with some openGL optimizing

--- src/main/java/org/educationalProject/surfacePathfinder/ColorizedMapVisualizer.java
+++ src/main/java/org/educationalProject/surfacePathfinder/ColorizedMapVisualizer.java
@@ -6,40 +6,46 @@ import io.github.jdiemke.triangulation.Triangle2D;
 public class ColorizedMapVisualizer extends MapVisualizer {
 
 	protected void drawTriangles(){
-
+		glBegin(GL_TRIANGLES);	
+		float color;
 		for(Triangle2D triangle : triangles){
 			
 			Point a = (Point)triangle.a;
 			Point b = (Point)triangle.b;
 			Point c = (Point)triangle.c;
 			
-			glBegin(GL_TRIANGLES);	
-			  float color;
-			  
-			  color = normalizeColor(a.alt, minAlt, maxAlt);
-			  glColor3f(1, color, 0);						  
-			  glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY)); 
+			color = normalizeColor(a.alt, minAlt, maxAlt);
+			glColor3f(1, color, 0);						  
+			glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY)); 
 
-			  color = normalizeColor(b.alt, minAlt, maxAlt);
-			  glColor3f(1, color, 0);						  
-			  glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));  
+			color = normalizeColor(b.alt, minAlt, maxAlt);
+			glColor3f(1, color, 0);						  
+			glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));  
 
-			  color = normalizeColor(c.alt, minAlt, maxAlt);
-			  glColor3f(1, color, 0);						  
-			  glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY)); 
-			glEnd();
+			color = normalizeColor(c.alt, minAlt, maxAlt);
+			glColor3f(1, color, 0);						  
+			glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY)); 
 			
-			glColor3f(0.5f, 0, 0.5f);
-			glLineWidth(1f);
-	        glBegin(GL_LINES);
-	          glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY)); 
-	          glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));  
-	          glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));
-	          glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
-	          glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
-	          glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY));
-	        glEnd();
 		}
+		glEnd();
+
+        glBegin(GL_LINES);
+		glColor3f(0.5f, 0, 0.5f);
+		glLineWidth(1f);
+		for(Triangle2D triangle : triangles){
+			
+			Point a = (Point)triangle.a;
+			Point b = (Point)triangle.b;
+			Point c = (Point)triangle.c;
+			
+	        glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY)); 
+	        glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));  
+	        glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));
+	        glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
+	        glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
+	        glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY));
+		}
+        glEnd();
 	}
 	
 }
--- src/main/java/org/educationalProject/surfacePathfinder/DecolorizedMapVisualizer.java
+++ src/main/java/org/educationalProject/surfacePathfinder/DecolorizedMapVisualizer.java
@@ -6,35 +6,35 @@ import io.github.jdiemke.triangulation.Triangle2D;
 public class DecolorizedMapVisualizer extends MapVisualizer {
 
 	protected void drawTriangles(){
-
+		glLineWidth(1f);
+        glBegin(GL_LINES);
+        
 		for(Triangle2D triangle : triangles){
 			
 			Point a = (Point)triangle.a;
 			Point b = (Point)triangle.b;
 			Point c = (Point)triangle.c;
 			
-			glColor3f(0.5f, 0, 0.5f);
-			glLineWidth(1f);
-	        glBegin(GL_LINES);
-	          float color = normalizeColor(a.alt, minAlt, maxAlt);
-			  glColor3f(1, color, 0);	
-	          glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY));
+	        float color = normalizeColor(a.alt, minAlt, maxAlt);
+			glColor3f(1, color, 0);	
+	        glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY));
 	          
-	          color = normalizeColor(b.alt, minAlt, maxAlt);
-			  glColor3f(1, color, 0);	
-	          glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));  
-	          glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));
+	        color = normalizeColor(b.alt, minAlt, maxAlt);
+			glColor3f(1, color, 0);	
+	        glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));  
+	        glVertex2f(normalizeCoords(b.x,minX,maxX), normalizeCoords(b.y,minY,maxY));
 	          
-	          color = normalizeColor(c.alt, minAlt, maxAlt);
-			  glColor3f(1, color, 0);	
-	          glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
-	          glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
+	        color = normalizeColor(c.alt, minAlt, maxAlt);
+			glColor3f(1, color, 0);	
+	        glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
+	        glVertex2f(normalizeCoords(c.x,minX,maxX), normalizeCoords(c.y,minY,maxY));
 	          
-	          color = normalizeColor(a.alt, minAlt, maxAlt);
-			  glColor3f(1, color, 0);	
-	          glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY));
-	        glEnd();
+	        color = normalizeColor(a.alt, minAlt, maxAlt);
+			glColor3f(1, color, 0);	
+	        glVertex2f(normalizeCoords(a.x,minX,maxX), normalizeCoords(a.y,minY,maxY));
+	        
 		}
+		glEnd();
 	}
 	
 }
