28d6ca1dab88a46a0d2d2bc4cfa7c65d29bbc8f4
kazuare <kazuare@mail.ru>
2017-04-11 22:37:06 +0300

    remake integer graph into point graph, remove id's of points

--- src/main/java/org/educationalProject/surfacePathfinder/Point.java
+++ src/main/java/org/educationalProject/surfacePathfinder/Point.java
@@ -4,13 +4,28 @@ import io.github.jdiemke.triangulation.Vector2D;
 
 public class Point extends Vector2D {
 	double alt;
-	int id;
 	
 	public Point(double x, double y, double alt, int id){
 		super(x, y);
 		this.alt = alt;
-		this.id = id;
+	}
+
+	public boolean equals(Point b){
+		if (this == b)
+			return true;
+		return (x == b.x) && (y == b.y);
 	}
 	
+	@Override
+	public boolean equals(Object obj){
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point b = (Point)obj;
+		return (x == b.x) && (y == b.y);
+	}
 	
 }
