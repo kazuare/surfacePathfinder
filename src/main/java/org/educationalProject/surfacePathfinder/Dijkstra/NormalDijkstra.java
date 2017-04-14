13bd89505209412c96c004322b76c1bc667ad0d9
kazuare <kazuare@mail.ru>
2017-04-14 03:17:06 +0300

    fix hillDijkstra algorithm

--- src/main/java/org/educationalProject/surfacePathfinder/Dijkstra/NormalDijkstra.java
+++ src/main/java/org/educationalProject/surfacePathfinder/Dijkstra/NormalDijkstra.java
@@ -10,7 +10,21 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
 public class NormalDijkstra extends Dijkstra{
-	
+	public void process(Point currentNode, HashMap<Point,Route> routes){
+		//System.out.println(currentNode + " is being  processed");
+		HashSet<IdAndLength> currentNeighbours = neighbours.get(currentNode);
+		//System.out.println("neighbours count: " + currentNeighbours.size());
+		if(currentNeighbours == null) return;
+		for(IdAndLength neighbour : currentNeighbours)
+			if(notVisited(neighbour.id))
+				if(routes.get(neighbour.id).length > routes.get(currentNode).length + neighbour.length){
+					Route newRoute = routes.get(currentNode).copy();
+					newRoute.append(neighbour.id);
+					newRoute.length = routes.get(currentNode).length + neighbour.length; 
+					routes.put(neighbour.id, newRoute);			
+				}
+			
+	}
 	public Point findMin(HashMap<Point,Route> routes){
 		Point minId = null;
 		
