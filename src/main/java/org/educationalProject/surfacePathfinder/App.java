75366058d9039e6d0e50af6b71d18dc0af172570
stalker-fc <stalker-fc@yandex.ru>
2017-04-19 22:04:02 +0300

    create test-files

--- src/main/java/org/educationalProject/surfacePathfinder/App.java
+++ src/main/java/org/educationalProject/surfacePathfinder/App.java
@@ -1,145 +1,18 @@
 package org.educationalProject.surfacePathfinder;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Vector;
+import org.educationalProject.surfacePathfinder.test.SerejaTest;
+import org.educationalProject.surfacePathfinder.test.YegorTest;
 
-import org.educationalProject.surfacePathfinder.Dijkstra.Dijkstra;
-import org.educationalProject.surfacePathfinder.Dijkstra.HillDijkstra;
-import org.educationalProject.surfacePathfinder.Dijkstra.Route;
-import org.educationalProject.surfacePathfinder.path.AStarPathFind;
-import org.educationalProject.surfacePathfinder.path.DijkstraPathFind;
-import org.educationalProject.surfacePathfinder.timing.NanoClock;
-import org.educationalProject.surfacePathfinder.timing.TicTocException;
-import org.educationalProject.surfacePathfinder.visualization.ColorizedMapVisualizer;
-import org.educationalProject.surfacePathfinder.visualization.PathVisualizer;
-import org.educationalProject.surfacePathfinder.visualization.SwingWindow;
-import org.jgrapht.alg.shortestpath.AStarShortestPath;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.SimpleWeightedGraph;
-
-import io.github.jdiemke.triangulation.NotEnoughPointsException;
-import io.github.jdiemke.triangulation.Triangle2D;
-import io.github.jdiemke.triangulation.Vector2D;
 /*
  * Main class that uses other classes to get job done. 
  * */
 public class App {
 
 	public static void main(String[] args) {
-		try{
-			double resultingTime;
-			// We will used this clock to measure time through all the phases
-			NanoClock clock = new NanoClock();
-			
-			// Obj file reading
-			clock.tic();
-			//Vector<Vector2D> points = ObjFileParser.getPoints("C:\\digdes\\map.obj");
-			Vector<Vector2D> points = ObjFileParser.getPoints("/home/merlin/DigDes/map.obj");
-			resultingTime = clock.tocd();
-			System.out.println("Reading is finished, phase duration is: " + resultingTime);
-			
-			// Points triangulating
-			clock.tic();
-			List<Triangle2D> triangles = Triangulator.triangulate(points);
-			resultingTime = clock.tocd();
-			System.out.println("Triangulation is finished, phase duration is: " + resultingTime);
-			
-			// Triangles -> graph convertion. Some edges are deleted if they have high altitude delta
-			clock.tic();
-			SimpleWeightedGraph<Point,DefaultWeightedEdge> graph = TrianglesToGraphConverter.convert(triangles);
-			resultingTime = clock.tocd();
-			System.out.println("Graph building is finished, phase duration is: " + resultingTime);
-			
-			SimpleWeightedGraph<Point,DefaultWeightedEdge> graphWithoutPenalty = TrianglesToGraphConverter.convertWithoutAltitudePenalty(triangles);
-			
-			
-			//Finding the shortest path
-			clock.tic();
-			AStarShortestPath<Point,DefaultWeightedEdge> astar = 
-					new AStarShortestPath<Point,DefaultWeightedEdge>(
-						graph,
-						new EuclidianEuristic<Point>()
-					);
-			resultingTime = clock.tocd();
-			System.out.println("Euristic building is finished, phase duration is: " + resultingTime);
-			
-			clock.tic();
-			Point a = (Point)points.get((int)(Math.random()*points.size()));
-			Point b = (Point)points.get((int)(Math.random()*points.size()));
-			List<Point> nodes = astar.getPath(a, b).getVertexList();
-			resultingTime = clock.tocd();
-			System.out.println("A* from jgrapht is finished, phase duration is: " + resultingTime);
-			
-			clock.tic();
-			DijkstraShortestPath<Point,DefaultWeightedEdge> alternative = new DijkstraShortestPath<Point,DefaultWeightedEdge>(graph);
-			List<Point> dijkstraNodes = alternative.getPath(a, b).getVertexList();
-			resultingTime = clock.tocd();
-			System.out.println("Dijkstra from jgrapht is finished, phase duration is: " + resultingTime);
-			
-			/*clock.tic();
-			Dijkstra x = new HillDijkstra();
-			x.init(graph);
-			//x.init(graphWithoutPenalty);
-			HashMap<Point, Route> routes = x.run(a);
-			List<Point> r = routes.get(b).vertices;
-			resultingTime = clock.tocd();
-			System.out.println("Alternative 2 algo is finished, phase duration is: " + resultingTime);*/
-
-
-			clock.tic();
-			DijkstraPathFind dijkstraPathFind = new DijkstraPathFind();
-			List<Point> dijkstraNodes1 = dijkstraPathFind.getShortestPath(graph, a, b);
-			resultingTime = clock.tocd();
-			System.out.println("Sereja`s dijkstra is finished, phase duration is: " + resultingTime);
-
-			clock.tic();
-			AStarPathFind aStarPathFind = new AStarPathFind();
-			List<Point> aStarNodes = aStarPathFind.getShortestPath(graph, a, b);
-			resultingTime = clock.tocd();
-			System.out.println("Sereja`s A* is finished, phase duration is: " + resultingTime);
-
-
-
-			//Checking algorithm correctness
-			boolean equal = true;
-			if(dijkstraNodes1.size()==dijkstraNodes.size()){
-				for(int i = 0; i < dijkstraNodes.size(); i++)
-					if(!dijkstraNodes.get(i).equals(dijkstraNodes1.get(i)))
-						equal = false;
-			}else{
-				equal = false;
-			}
-			System.out.println("Jgrapht`s dijkstra and Sereja`s dijkstra are equal: " + (""+equal).toUpperCase());
-			
-			//Visualizing
-			ColorizedMapVisualizer vis1 = new ColorizedMapVisualizer();
-			vis1.setData(triangles, nodes, graph);
-			SwingWindow.start(vis1, 800, 600, "Jgrapht`s A* map");
-			
-			//Visualizing
-			ColorizedMapVisualizer vis2 = new ColorizedMapVisualizer();
-			vis2.setData(triangles, aStarNodes, graph);
-			SwingWindow.start(vis2, 800, 600, "Sereja`s A* map");
-			
-			PathVisualizer vis3 = new PathVisualizer();
-			vis3.setData(nodes, points);
-			SwingWindow.start(vis3, 800, vis3.calculateWindowHeight(800), "Jgrapht`s A*");
-
-			PathVisualizer vis4 = new PathVisualizer();
-			vis4.setData(aStarNodes, points);
-			SwingWindow.start(vis4, 800, vis4.calculateWindowHeight(800), " Sereja`s A*");
-			
-			System.out.println("end");			
-		}catch(IOException e){
-			System.out.println("Problem with file reading accured!");
-		}catch(TicTocException e){
-			System.out.println("There is a problem with time measuring code. toc is executed before tic.");
-		} catch (NotEnoughPointsException e) {
-			System.out.println("Not enough points to triangulate");
-		}
+		//SerejaTest serejaTest = new SerejaTest();
+		//serejaTest.test();
+		YegorTest yegorTest = new YegorTest();
+		yegorTest.test();
 	}
 
 }
