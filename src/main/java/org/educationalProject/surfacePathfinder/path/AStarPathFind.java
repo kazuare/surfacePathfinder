6b76a606e3842cddd5464a29be3deb2c2e4ed5e8
stalker-fc <stalker-fc@yandex.ru>
2017-04-26 04:16:55 +0300

    rewrited algorithm, now path is correct

--- src/main/java/org/educationalProject/surfacePathfinder/path/AStarPathFind.java
+++ src/main/java/org/educationalProject/surfacePathfinder/path/AStarPathFind.java
@@ -1,6 +1,7 @@
 package org.educationalProject.surfacePathfinder.path;
 
 import org.educationalProject.surfacePathfinder.Point;
+import org.jgrapht.WeightedGraph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 import org.jgrapht.util.FibonacciHeap;
@@ -11,32 +12,54 @@ import java.util.*;
 
 public class AStarPathFind {
 
-    private SimpleWeightedGraph<Point, DefaultWeightedEdge> graph;
+    private WeightedGraph<Point, DefaultWeightedEdge> graph;
+    private PriorityQueue<DistancePoint> unSettledNodes;
     private Set<Point> settledNodes;
+<<<<<<< Updated upstream
     private PriorityQueue<DistancePoint> unSettledNodes;
     private Map<Point, Point> predecessors;
     private Map<Point, Double> gScore;
     private Map<Point, Double> fScore;
+=======
+    private Map<Point, Double> gScore;
+    private Map<Point, Double> hScore;
+    private Map<Point, Double> fScore;
+    private Map<Point, Point> predecessors;
+    private List<Point> shortestPath;
+>>>>>>> Stashed changes
     private Point source;
     private Point destination;
 
-    public List<Point> getShortestPath(SimpleWeightedGraph<Point, DefaultWeightedEdge> graph, Point source, Point destination) {
-        this.init(graph, source, destination);
-        this.findPath();
-        return this.retrievalPath();
-    }
+    public static Comparator<DistancePoint> comparator = new Comparator<DistancePoint>() {
+        @Override
+        public int compare(DistancePoint a, DistancePoint b) {
+            return (int)Math.signum(a.distance - b.distance);
+        }
+    };
 
-    private void init(SimpleWeightedGraph<Point, DefaultWeightedEdge> graph, Point source, Point destination) {
+    public List<Point> getShortestPath(WeightedGraph<Point, DefaultWeightedEdge> graph, Point source, Point destination){
+        initialize(graph, source, destination);
+        findPath();
+        retrievePath();
+        return shortestPath;
+    }
+    private void initialize(WeightedGraph<Point, DefaultWeightedEdge> graph, Point source, Point destination){
         this.graph = graph;
         this.source = source;
         this.destination = destination;
+
         settledNodes = new HashSet<Point>();
         unSettledNodes = new PriorityQueue<DistancePoint>(comparator);
         gScore = new HashMap<Point, Double>();
+<<<<<<< Updated upstream
+=======
+        hScore = new HashMap<Point, Double>();
+>>>>>>> Stashed changes
         fScore = new HashMap<Point, Double>();
         predecessors = new HashMap<Point, Point>();
 
         gScore.put(this.source, 0.0);
+<<<<<<< Updated upstream
         fScore.put(this.source, getHeuristic(this.source, this.source));
         unSettledNodes.add(new DistancePoint(this.source, fScore.get(this.source)));
     }
@@ -67,6 +90,38 @@ public class AStarPathFind {
                 predecessors.remove(neighbor);
                 gScore.remove(neighbor);
                 fScore.remove(neighbor);
+=======
+        hScore.put(this.source, getHScore(this.source));
+        fScore.put(this.source, getGScore(this.source) + getHScore(this.source));
+        unSettledNodes.add(new DistancePoint(this.source, getFScore(this.source)));
+    }
+
+    private void findPath(){
+        while (!unSettledNodes.isEmpty()){
+            Point current = unSettledNodes.poll().point;
+            if (current.equals(destination))
+                return;
+            settledNodes.add(current);
+            List<Point> neighbors = getNeighbors(current);
+            for (Point neighbor : neighbors) {
+                if (settledNodes.contains(neighbor))
+                    continue;
+                Double tentativeScore = getGScore(current) + getDistance(current, neighbor);
+                if (tentativeScore >= getGScore(neighbor))
+                    continue;
+                if(isUnSettled(neighbor)){
+                    unSettledNodes.remove(new DistancePoint(neighbor, getFScore(neighbor)));
+                    predecessors.remove(neighbor);
+                    gScore.remove(neighbor);
+                    hScore.remove(neighbor);
+                    fScore.remove(neighbor);
+                }
+                gScore.put(neighbor, tentativeScore);
+                hScore.put(neighbor, getHScore(neighbor));
+                fScore.put(neighbor, getGScore(neighbor) + getHScore(neighbor));
+                predecessors.put(neighbor, current);
+                unSettledNodes.add(new DistancePoint(neighbor, getFScore(neighbor)));
+>>>>>>> Stashed changes
             }
             gScore.put(neighbor, tentativeScore);
             fScore.put(neighbor, getHeuristic(node, neighbor));
@@ -74,6 +129,7 @@ public class AStarPathFind {
             predecessors.put(neighbor, node);
         }
     }
+<<<<<<< Updated upstream
 
     private Double getHeuristic(Point current, Point next) {
         Double g = gScore.get(current);
@@ -97,10 +153,21 @@ public class AStarPathFind {
         } else {
             return g;
         }
+=======
+    private Double getDistance(Point src, Point target) {
+        if (src.equals(target))
+            return 0.0;
+        DefaultWeightedEdge e = graph.getEdge(src, target);
+        if (e == null)
+            return Double.MAX_VALUE;
+        else
+            return graph.getEdgeWeight(e);
+>>>>>>> Stashed changes
     }
 
-    private List<Point> getNeighbors(Point node) {
+    private List<Point> getNeighbors(Point current) {
         List<Point> neighbors = new ArrayList<Point>();
+<<<<<<< Updated upstream
         Set<DefaultWeightedEdge> edges = graph.edgesOf(node);
         for (DefaultWeightedEdge edge : edges) {
             Point source = graph.getEdgeSource(edge);
@@ -110,10 +177,21 @@ public class AStarPathFind {
             }else if (target.equals(node) && !isSettled(source)){
                 neighbors.add(source);
             }
+=======
+        Set<DefaultWeightedEdge> edges = graph.edgesOf(current);
+        for (DefaultWeightedEdge e : edges) {
+            Point src = graph.getEdgeSource(e);
+            Point target = graph.getEdgeTarget(e);
+            if (src.equals(current))
+                neighbors.add(target);
+            else
+                neighbors.add(src);
+>>>>>>> Stashed changes
         }
         return neighbors;
     }
 
+<<<<<<< Updated upstream
     public static Comparator<DistancePoint> comparator = new Comparator<DistancePoint>() {
         @Override
         public int compare(DistancePoint a, DistancePoint b) {
@@ -124,18 +202,45 @@ public class AStarPathFind {
     private Point getMinimum() {
         Point minimum = unSettledNodes.peek().point;
         return minimum;
+=======
+    private boolean isUnSettled(Point current) { return fScore.containsKey(current); }
+    private Double getGScore(Point current) {
+        Double g = gScore.get(current);
+        if (g == null)
+            return Double.MAX_VALUE;
+        else
+            return  g;
+>>>>>>> Stashed changes
     }
-
-    private boolean isSettled(Point node) {
-        return settledNodes.contains(node);
+    private Double getFScore(Point current) {
+        Double f = fScore.get(current);
+        if (f == null)
+            return Double.MAX_VALUE;
+        else
+            return f;
     }
+<<<<<<< Updated upstream
     private boolean isUnSettled(Point node) {return fScore.containsKey(node); }
 
     private List<Point> retrievalPath() {
         List<Point> shortestPath = new LinkedList<Point>();
+=======
+    private Double getHScore(Point current){
+        Double h = hScore.get(current);
+        if (h == null)
+            return Math.sqrt((destination.x - current.x) * (destination.x - current.x)
+                    + (destination.y - current.y) * (destination.y - current.y)
+                    + (destination.alt - current.alt) * (destination.alt - current.alt));
+
+        return h;
+    }
+    private void retrievePath() {
+        shortestPath = new LinkedList<Point>();
+>>>>>>> Stashed changes
         Point step = this.destination;
         if (predecessors.get(step) == null) {
-            return null;
+            shortestPath = null;
+            return;
         }
         shortestPath.add(step);
         while (predecessors.get(step) != null) {
@@ -143,6 +248,16 @@ public class AStarPathFind {
             shortestPath.add(step);
         }
         Collections.reverse(shortestPath);
-        return shortestPath;
+    }
+    public Double getLengthOfPath() {
+        Double length = 0.0;
+        if (shortestPath == null)
+            return 0.0;
+
+        for (int j = 0; j < shortestPath.size() - 1; j++) {
+            DefaultWeightedEdge e = graph.getEdge(shortestPath.get(j), shortestPath.get(j + 1));
+            length += (double) graph.getEdgeWeight(e);
+        }
+        return length;
     }
 }
--- src/main/java/org/educationalProject/surfacePathfinder/test/SerejaTest.java
+++ src/main/java/org/educationalProject/surfacePathfinder/test/SerejaTest.java
@@ -21,7 +21,39 @@ import io.github.jdiemke.triangulation.NotEnoughPointsException;
 import io.github.jdiemke.triangulation.Triangle2D;
 import io.github.jdiemke.triangulation.Vector2D;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Vector;
+
+import org.educationalProject.surfacePathfinder.*;
+import org.educationalProject.surfacePathfinder.Dijkstra.Dijkstra;
+import org.educationalProject.surfacePathfinder.Dijkstra.HillDijkstra;
+import org.educationalProject.surfacePathfinder.Dijkstra.Route;
+import org.educationalProject.surfacePathfinder.path.AStarPathFind;
+import org.educationalProject.surfacePathfinder.timing.NanoClock;
+import org.educationalProject.surfacePathfinder.timing.TicTocException;
+import org.educationalProject.surfacePathfinder.visualization.ColorizedMapVisualizer;
+import org.educationalProject.surfacePathfinder.visualization.PathVisualizer;
+import org.educationalProject.surfacePathfinder.visualization.SwingWindow;
+import org.jgrapht.alg.shortestpath.AStarShortestPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import io.github.jdiemke.triangulation.NotEnoughPointsException;
+import io.github.jdiemke.triangulation.Triangle2D;
+import io.github.jdiemke.triangulation.Vector2D;
+import org.educationalProject.surfacePathfinder.Dijkstra.OnlineNormalDijkstra;
+import org.educationalProject.surfacePathfinder.onlineTriangulation.DomainBasedJdiemkeTriangulator;
+import org.educationalProject.surfacePathfinder.onlineTriangulation.GraphProxy;
+import org.educationalProject.surfacePathfinder.onlineTriangulation.GreedyTriangulator;
+import org.educationalProject.surfacePathfinder.visualization.DecolorizedMapVisualizer;
+import org.educationalProject.surfacePathfinder.visualization.GraphVisualizer;
+
 public class SerejaTest {
+<<<<<<< Updated upstream
 
     //addition punishment in our distances
     static final double ALTITUDE_MULTIPLIER = 16;
@@ -134,13 +166,89 @@ public class SerejaTest {
             vis4.setData(aStarNodes, points);
             SwingWindow.start(vis4, 800, vis4.calculateWindowHeight(800), " Sereja`s A*");*/
 
+=======
+
+    //addition punishment in our distances
+    static final double ALTITUDE_MULTIPLIER = 4;
+    //is used to determine whether the edge is "bad" and should not be included
+    static final double COS_THRESHOLD = 0.5;
+    static final double TRIANGULATION_RADIUS = 0.75; //this value seems to work good.
+
+
+    public static void setup(){
+        EdgeWeighter.setParams(ALTITUDE_MULTIPLIER);
+    }
+    public static void test(){
+        try{
+            double resultingTime;
+            // We will used this clock to measure time through all the phases
+            NanoClock clock = new NanoClock();
+
+            // Obj file reading
+            clock.tic();
+            Vector<Point> realPoints = ObjFileParser.getPoints2("/home/merlin/DigDes/map.obj");
+            resultingTime = clock.tocd();
+            System.out.println("Reading is finished, phase duration is: " + resultingTime);
+            System.out.println("N is: " + realPoints.size());
+
+            GraphProxy graph = new GraphProxy(TRIANGULATION_RADIUS, realPoints, "UnsafeJdiemkeTriangulator");
+            GraphProxy graph1 = new GraphProxy(TRIANGULATION_RADIUS, realPoints, "DomainBasedJdiemkeTriangulator");
+            GraphProxy graph2 = new GraphProxy(TRIANGULATION_RADIUS, realPoints, "GreedyTriangulator");
+
+            Point a = realPoints.get((int)(Math.random()*realPoints.size()));
+            Point b = realPoints.get((int)(Math.random()*realPoints.size()));
+
+            showAStar(clock, graph, a, b, "Unsafe triangulation");
+            //graph.visualizeDebug();
+            //showDijkstra(clock, graph1, a, b);
+
+>>>>>>> Stashed changes
             System.out.println("end");
         }catch(IOException e){
             System.out.println("Problem with file reading accured!");
         }catch(TicTocException e){
             System.out.println("There is a problem with time measuring code. toc is executed before tic.");
+<<<<<<< Updated upstream
         } catch (NotEnoughPointsException e) {
             System.out.println("Not enough points to triangulate");
         }
+=======
+        }
+
+    }
+
+    public static void showAStar(NanoClock clock, GraphProxy graph, Point a, Point b) throws TicTocException{
+        showAStar(clock, graph, a, b, "Astar");
+    }
+
+    public static void showAStar(NanoClock clock, GraphProxy graph, Point a, Point b, String windowName) throws TicTocException{
+        double resultingTime;
+        clock.tic();
+        AStarShortestPath<Point, DefaultWeightedEdge> jgraphtAStar =
+                new AStarShortestPath<Point,DefaultWeightedEdge>(
+                        graph,
+                        new EuclidianEuristicWithAltitude<Point>(ALTITUDE_MULTIPLIER)
+                );
+        List<Point> jgraphtNodes = jgraphtAStar.getPath(a, b).getVertexList();
+        resultingTime = clock.tocd();
+        System.out.println("Jgrapht algo is finished, phase duration is: " + resultingTime);
+        System.out.println("Path length is: " + jgraphtAStar.getPath(a, b).getWeight());
+
+        clock.tic();
+        AStarPathFind aStarPathFind = new AStarPathFind();
+        List<Point> serejaNodes = aStarPathFind.getShortestPath(graph, a, b);
+        resultingTime = clock.tocd();
+        System.out.println("Sereja algo is finished, phase duration is: " + resultingTime);
+        System.out.println("Path length is: " + aStarPathFind.getLengthOfPath());
+
+
+        //Visualizing
+        DecolorizedMapVisualizer vis = new DecolorizedMapVisualizer();
+        vis.setData(graph, jgraphtNodes);
+        SwingWindow.start(vis, 700, 700, "Jgrapht");
+        DecolorizedMapVisualizer vis1 = new DecolorizedMapVisualizer();
+        vis1.setData(graph, jgraphtNodes);
+        SwingWindow.start(vis, 700, 700, "Sereja");
+>>>>>>> Stashed changes
     }
 }
