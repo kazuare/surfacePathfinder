99f3ace766b2a89e4ff074fe28ddd4e5808d7596
stalker-fc <stalker-fc@yandex.ru>
2017-05-18 22:18:45 +0300

    delete odd code

--- src/main/java/org/educationalProject/surfacePathfinder/onlineTriangulation/ModifiedJdiemkeWithVisualization.java
+++ src/main/java/org/educationalProject/surfacePathfinder/onlineTriangulation/ModifiedJdiemkeWithVisualization.java
@@ -22,8 +22,6 @@ import io.github.jdiemke.triangulation.Vector2D;
 
 public class ModifiedJdiemkeWithVisualization extends ModifiedJdiemke implements OnlineTriangulator{
 
-    private double maxOfAnyCoordinate = 0;
-    private Triangle2D superTriangle;
     private MainDemoWindow demo;
 
     public ModifiedJdiemkeWithVisualization(SimpleWeightedGraph<Point, DefaultWeightedEdge> graph,ArrayList<Point> points,
@@ -32,80 +30,16 @@ public class ModifiedJdiemkeWithVisualization extends ModifiedJdiemke implements
         this.demo = demo;
     }
 
-    public void setMaxCoordinate(Set<Point> points){
-        for (Point p : points)
-            maxOfAnyCoordinate = Math.max(Math.max(p.x, p.y), maxOfAnyCoordinate);
-        maxOfAnyCoordinate *= 48;
-    }
 
-    @Override
-    public SimpleWeightedGraph<Point, DefaultWeightedEdge> init(Point center){
-        setMaxCoordinate(graph.vertexSet());
-        initTriangulation();
-        return update(center);
-    };
     @Override
     public SimpleWeightedGraph<Point, DefaultWeightedEdge> update(Point center){
         int oldSize = graph.edgeSet().size();
-
-        ArrayList<Point> neighbours = getNearbyPoints(center);
-
-        Set<DefaultWeightedEdge> toRemove = new HashSet<DefaultWeightedEdge>(graph.edgeSet());
-        for(DefaultWeightedEdge e : toRemove)
-            graph.removeEdge(e);
-
-        for(Point p : neighbours)
-            addPoint(p);
-
-        for(Triangle2D triangle : triangleSoup.getTriangles()){
-            if(triangle.hasVertex(superTriangle.a))
-                continue;
-            if(triangle.hasVertex(superTriangle.b))
-                continue;
-            if(triangle.hasVertex(superTriangle.c))
-                continue;
-
-            Point a = (Point)triangle.a;
-            Point b = (Point)triangle.b;
-            Point c = (Point)triangle.c;
-
-            DefaultWeightedEdge e = graph.addEdge(a, b);
-            if(e!=null)
-                graph.setEdgeWeight(e, EdgeWeighter.edgeWeight(a, b));
-
-            e = null;
-            e = graph.addEdge(a, c);
-            if(e!=null)
-                graph.setEdgeWeight(e, EdgeWeighter.edgeWeight(a, c));
-
-            e = null;
-            e = graph.addEdge(c, b);
-            if(e!=null)
-                graph.setEdgeWeight(e, EdgeWeighter.edgeWeight(c, b));
-
-        }
-
-        try {
-            setProcessedPoints(neighbours, QuickHull.findHull(neighbours));
-        } catch (Exception e1) {
-            e1.printStackTrace();
-        }
+        graph = super.update(center);
         if (oldSize < graph.edgeSet().size()) {
             displayGraph();
         }
         return graph;
     }
-
-    public void initTriangulation(){
-        Vector2D p1 = new Vector2D(0d, maxOfAnyCoordinate);
-        Vector2D p2 = new Vector2D(maxOfAnyCoordinate, 0d);
-        Vector2D p3 = new Vector2D(-maxOfAnyCoordinate, -maxOfAnyCoordinate);
-
-        superTriangle = new Triangle2D(p1, p2, p3);
-
-        triangleSoup.add(superTriangle);
-    }
-
 
     public void displayGraph() {
         demo.setGraph((SimpleWeightedGraph<Point, DefaultWeightedEdge>) graph.clone());
