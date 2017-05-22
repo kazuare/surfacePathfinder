39856809a9943d42059a50fb9972441ba757d222
stalker-fc <stalker-fc@yandex.ru>
2017-05-22 21:52:00 +0300

    update finding stoppoint

--- src/main/java/org/educationalProject/surfacePathfinder/path/StopPointSearcher.java
+++ src/main/java/org/educationalProject/surfacePathfinder/path/StopPointSearcher.java
@@ -2,6 +2,9 @@ package org.educationalProject.surfacePathfinder.path;
 
 import org.educationalProject.surfacePathfinder.Point;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,18 +13,28 @@ public class StopPointSearcher implements Runnable {
     protected AtomicInteger stopPointSource;
     protected AtomicInteger stopPointDestination;
     protected AtomicBoolean stop;
+    protected ConcurrentHashMap<Point, Double> distancesSource;
+    protected ConcurrentHashMap<Point, Double> distancesDestination;
     protected CopyOnWriteArrayList<Point> settledNodesSource;
     protected CopyOnWriteArrayList<Point> settledNodesDestination;
+    protected CopyOnWriteArrayList<Point> intersection;
 
     public StopPointSearcher(AtomicInteger stopPointSource, AtomicInteger stopPointDestination,
                              AtomicBoolean stop,
                              CopyOnWriteArrayList<Point> settledNodesSource,
-                             CopyOnWriteArrayList<Point> settledNodesDestination) {
+                             CopyOnWriteArrayList<Point> settledNodesDestination,
+                             CopyOnWriteArrayList<Point> intersection,
+                             ConcurrentHashMap<Point, Double> distancesSource,
+                             ConcurrentHashMap<Point, Double> distancesDestination
+                             ) {
         this.stopPointSource = stopPointSource;
         this.stopPointDestination = stopPointDestination;
         this.stop = stop;
         this.settledNodesSource = settledNodesSource;
         this.settledNodesDestination = settledNodesDestination;
+        this.intersection = intersection;
+        this.distancesSource = distancesSource;
+        this.distancesDestination = distancesDestination;
     }
 
 
@@ -31,13 +44,13 @@ public class StopPointSearcher implements Runnable {
 
         if (settledNodesSource.size() > oldSizeNodesSource) {
             currentSizeNodesSource = settledNodesSource.size();
-            for (int i = oldSizeNodesSource; i < currentSizeNodesSource; i++) {
+            for (int i = 0; i < currentSizeNodesSource; i++) {
                 Point tmp = settledNodesSource.get(i);
                 if (settledNodesDestination.contains(tmp)) {
                     stop.set(true);
-                    stopPointSource.set(i);
-                    stopPointDestination.set(settledNodesDestination.indexOf(tmp));
-                    return;
+                    //stopPointSource.set(i);
+                    //stopPointDestination.set(settledNodesDestination.indexOf(tmp));
+                    intersection.add(tmp);
                 }
             }
             oldSizeNodesSource = currentSizeNodesSource;
@@ -45,16 +58,32 @@ public class StopPointSearcher implements Runnable {
 
         if (settledNodesDestination.size() > oldSizeNodesDestination) {
             currentSizeNodesDestination = settledNodesDestination.size();
-            for (int i = oldSizeNodesDestination; i < currentSizeNodesDestination; i++) {
+            for (int i = 0; i < currentSizeNodesDestination; i++) {
                 Point tmp = settledNodesDestination.get(i);
                 if (settledNodesSource.contains(tmp)) {
                     stop.set(true);
-                    stopPointDestination.set(i);
-                    stopPointSource.set(settledNodesSource.indexOf(tmp));
-                    return;
+                    //stopPointSource.set(i);
+                    //stopPointDestination.set(settledNodesDestination.indexOf(tmp));
+                    intersection.add(tmp);
                 }
             }
             oldSizeNodesDestination = currentSizeNodesDestination;
+        }
+        if (stop.get()) {
+            System.out.println("intersection size " + intersection.size());
+            for (int i = 0; i < intersection.size(); i++)
+                System.out.println("int " + intersection.get(i).toString());
+            Double dist = Double.MAX_VALUE;
+            Point res = new Point(0.0, 0.0, 0.0);
+            for (Point p : intersection) {
+                Double sum = distancesSource.get(p) + distancesDestination.get(p);
+                if (sum < dist) {
+                    dist = sum;
+                    res = p;
+                }
+            }
+            stopPointSource.set(settledNodesSource.indexOf(res));
+            stopPointDestination.set(settledNodesDestination.indexOf(res));
         }
     }
 
